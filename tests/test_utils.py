446866e68a83b8866e5b75710c1981429319ebbd
Sachaa-Thanasius <i.like.ike101@gmail.com>
2024-04-09 14:41:10 -0400

    Fix bad import in test_utils.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,23 +1,10 @@
 from typing import Protocol, runtime_checkable
 
-from __experimental__ import _peekable, _utils
-
-
-def test_copy_annotations() -> None:
-    @runtime_checkable
-    class BaseProtocol(Protocol):
-        def __call__(self, inp: int, *, out: str) -> str: ...
-
-    def base(inp: int, *, out: str) -> str: ...
-
-    @_utils.copy_annotations(base)  # Error expected; ignore.
-    def derived(inp, *, out): ...  # Error expected; ignore.
-
-    assert isinstance(derived, BaseProtocol)
+from __experimental__._utils import misc, peekable
 
 
 def test_peekable() -> None:
-    peekable_gen = _peekable.Peekable(i * 2 for i in range(20))
+    peekable_gen = peekable.Peekable(i * 2 for i in range(20))
 
     assert not peekable_gen._cache
     assert peekable_gen.has_more()
